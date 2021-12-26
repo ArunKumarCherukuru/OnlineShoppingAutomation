daba560fc32a3988fba683d507983d8424a6caf2
ArunKumarCherukuru <cherukuruarun@gmail.com>
2021-12-26 23:14:35 +0530

    Modified constants

--- src/main/java/com/SampleFramework/Utils/Constants.java
+++ src/main/java/com/SampleFramework/Utils/Constants.java
@@ -3,5 +3,9 @@ package com.SampleFramework.Utils;
 public interface Constants {
 	String url = "http://automationpractice.com/index.php";
 	String testDataFilePath = System.getProperty("user.dir") + "\\testData\\testData.xlsx";
-	String PageTitle ="";
+	String PageTitle = "";
+	String PageTitle2 = "";
+	String PageTitle3 ="";
+
+
 }
