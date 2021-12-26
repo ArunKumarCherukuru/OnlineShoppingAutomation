60ee92b747505f99a0167938c043e08d4e518ab4
ArunKumarCherukuru <cherukuruarun@gmail.com>
2021-12-26 23:02:42 +0530

    Added JavaScriptUtil class & modified constants page

--- src/main/java/com/SampleFramework/Utils/Constants.java
+++ src/main/java/com/SampleFramework/Utils/Constants.java
@@ -3,4 +3,5 @@ package com.SampleFramework.Utils;
 public interface Constants {
 	String url = "http://automationpractice.com/index.php";
 	String testDataFilePath = System.getProperty("user.dir") + "\\testData\\testData.xlsx";
+	String PageTitle ="";
 }
